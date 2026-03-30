3a448522c7a998754c0300610289b26370df8a7b
Gregory Amerson <gregory.amerson@liferay.com>
2026-03-30 14:37:35 -0500

    include dependencies

--- update-versions.sh
+++ update-versions.sh
@@ -6,4 +6,5 @@ find . -name "Chart.yaml" | while read -r chart_file; do
     # Use sed to append -999 to the version line if it doesn't already end in -999
     # This matches 'version: x.y.z' and turns it into 'version: x.y.z-999'
     sed -i '/^version: / { /-999$/! s/$/-999/ }' "$chart_file"
+    sed -i '/\sversion: / { /-999$/! s/$/-999/ }' "$chart_file"
 done
